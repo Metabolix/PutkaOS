2d26c3c2c29f8c6f4466f5c741a89b8e84ff87c6
Lauri Kenttä <lauri.kentta@gmail.com>
2007-02-20 16:20:13 +0000

    Omg log xD

---
+++ src/filesystem.c
@@ -0,0 +1,24 @@
+#include <pseudofsdriver.h>
+#include <filesystem.h>
+#include <malloc.h>
+
+#define MAX_FS_DRIVERS 16
+
+fs_mount_t list_of_fs_mount[MAX_FS_DRIVERS] = {
+	(fs_mount_t) pfs_mount,
+	0 /* Terminator */
+};
+
+struct fs *fs_mount(FILE *dev, uint_t mode)
+{
+	int i;
+	struct fs *retval;
+
+	/* Valitaan listasta oikea juttu */
+	for (i = 0; i < MAX_FS_DRIVERS && list_of_fs_mount[i]; ++i) {
+		if ((retval = list_of_fs_mount[i](dev, mode))) {
+			return retval;
+		}
+	}
+	return 0;
+}
