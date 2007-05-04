0f711c0cf9126c74e0af3293420ba84da687478e
Lauri Kenttä <lauri.kentta@gmail.com>
2007-05-04 17:58:51 +0000

    Jee, pilkku...

--- include/stdarg.h
+++ include/stdarg.h
@@ -5,9 +5,9 @@ typedef struct va_list_s {
 	void* va_ptr;
 } va_list;
 
-#define va_start(ap, argN) {ap.va_ptr = &argN + 1;}
-#define va_copy(dest, src) {dest.va_ptr = src.va_ptr;}
-#define va_arg(ap, type) *(type*)((int*)(ap.va_ptr = (int*)ap.va_ptr + ((sizeof(type)+sizeof(int)-1)/sizeof(int))) - ((sizeof(type)+sizeof(int)-1)/sizeof(int)))
-#define va_end(ap); {ap.va_ptr = 0;}
+#define va_start(ap, argN) ((ap.va_ptr = &argN + 1),(void)0)
+#define va_copy(dest, src) ((dest.va_ptr = src.va_ptr),(void)0)
+#define va_arg(ap, type) ((ap.va_ptr = ((int*)ap.va_ptr + ((sizeof(type)+sizeof(int)-1)/sizeof(int)))), *(type*)((int*)ap.va_ptr - ((sizeof(type)+sizeof(int)-1)/sizeof(int))))
+#define va_end(ap); ((ap.va_ptr = 0),(void)0)
 
 #endif
