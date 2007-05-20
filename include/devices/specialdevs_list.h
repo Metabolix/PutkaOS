fba2c50e517c10205b6ac80fe49241f628b7e4cf
Lauri Kenttä <lauri.kentta@gmail.com>
2007-05-20 19:04:44 +0000

    Varoitusviesti yhteen otsikkoon...

--- include/devices/specialdevs_list.h
+++ include/devices/specialdevs_list.h
@@ -1,4 +1,12 @@
+#ifndef MKSPECIALDEV
+
+#error MKSPECIALDEV not defined. Note: this is a special header and needs special knowledge.
+
+#else
+
 MKSPECIALDEV( null )
 MKSPECIALDEV( zero )
 //MKSPECIALDEV( random )
 //MKSPECIALDEV( urandom )
+
+#endif
