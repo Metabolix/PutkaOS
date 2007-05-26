a0c42417653d65dfb8c0314c3034dac891c56c50
Kai Lindholm <megantti@gmail.com>
2007-05-26 06:50:07 +0000

    make_image.sh kirjottaa uuden versionumeron

--- make_image.sh
+++ make_image.sh
@@ -5,8 +5,8 @@ mount -o loop putkaos.img putkaos
 mkdir -p putkaos/boot/grub/
 cp /boot/grub/stage1 putkaos/boot/grub/
 cp /boot/grub/stage2 putkaos/boot/grub/
-echo -e "default 0\ntimeout 5\ntitle=PutkaOS v0.001\nroot (fd0)\nkernel /kernel\nboot" > putkaos/boot/grub/menu.lst
-/sbin/grub --batch --device-map=/dev/null <<EOF
+echo -e "default 0\ntimeout 5\ntitle=PutkaOS v0.002\nroot (fd0)\nkernel /kernel\nboot" > putkaos/boot/grub/menu.lst
+/usr/sbin/grub --batch --device-map=/dev/null <<EOF
 device (fd0) putkaos.img
 root (fd0)
 setup (fd0)
