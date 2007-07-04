3078612ad430da3a5241f198600b2982aa4dbeb4
Lauri Kenttä <lauri.kentta@gmail.com>
2007-07-04 15:50:34 +0000

    __fixunsxfdi

--- src/build_tweaks.asm
+++ src/build_tweaks.asm
@@ -1,9 +1,13 @@
 [BITS 32]
 
+global ret_0
+
 global __stack_chk_fail
+global __fixunsxfdi
 
 __stack_chk_fail:
-	jmp ret_0
+__fixunsxfdi:
+	; jmp ret_0 ; suoraan perässä on, turha hyppiä
 
 ret_0:
 	xor eax, eax
